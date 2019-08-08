77288a143ae907626c2d7271de547ff04730ab82
Zubair <alizubair686@gmail.com>
2019-08-08 16:25:04 +0500

    new commit 08

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,8 +1,19 @@
 import React from "react";
 import imgReact from "../react.jpeg";
+import imgReads from "../reads2.png";
+import imgReads1 from "../reads1.png";
+import imgWould from "../would1.png";
+import imgWould1 from "../would2.png";
+import imgWould2 from "../would8.png";
 import imgNode from "../node.png";
+import imgNode1 from "../node1.jpg";
+import imgNode2 from "../node2.png";
 import imgNative from "../react-native.png";
-import imgb from "../b.jpg";
+import imgFlash from "../flash1.png";
+import imgFlash1 from "../flash2.png";
+import imgLeads from "../leads1.png";
+import imgLeads1 from "../leads2.png";
+import imgLeads2 from "../leads3.png";
 
 const Projects = () => {
   return (
@@ -11,12 +22,71 @@ const Projects = () => {
       <div className="row">
         <div className="col- col-md-6 col-lg-4 mb-4">
           <div class="card" style={{ height: "650px" }}>
-            <img
-              class="card-img-top"
-              src={imgReact}
-              alt="Card image cap"
-              style={{ height: "300px" }}
-            />
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                />
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src={imgWould}
+                    alt="First slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgWould1}
+                    alt="Second slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgWould2}
+                    alt="Third slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true" />
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true" />
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
             <div class="card-body text-center">
               <h5 class="card-title">Would you Rather App</h5>
               <p class="card-text">
@@ -35,12 +105,71 @@ const Projects = () => {
         </div>
         <div className="col- col-md-6 col-lg-4 mb-4">
           <div class="card" style={{ height: "650px" }}>
-            <img
-              class="card-img-top"
-              src={imgReact}
-              alt="Card image cap"
-              style={{ height: "300px" }}
-            />
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                />
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src={imgReact}
+                    alt="First slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgReads}
+                    alt="Second slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgReads1}
+                    alt="Third slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true" />
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true" />
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
             <div class="card-body text-center">
               <h5 class="card-title">My Reads App</h5>
               <p class="card-text">
@@ -62,12 +191,71 @@ const Projects = () => {
         </div>
         <div className="col- col-md-6 col-lg-4 mb-4">
           <div class="card" style={{ height: "650px" }}>
-            <img
-              class="card-img-top"
-              src={imgNode}
-              alt="Card image cap"
-              style={{ height: "300px" }}
-            />
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                />
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src={imgNode}
+                    alt="First slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgNode1}
+                    alt="Second slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgNode2}
+                    alt="Third slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true" />
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true" />
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
             <div class="card-body text-center">
               <h5 class="card-title">Vidly API</h5>
               <p class="card-text">
@@ -87,20 +275,79 @@ const Projects = () => {
         </div>
         <div className="col- col-md-6 col-lg-4 mb-4">
           <div class="card" style={{ height: "650px" }}>
-            <img
-              class="card-img-top"
-              src={imgNative}
-              alt="Card image cap"
-              style={{ height: "300px" }}
-            />
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                />
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src={imgNative}
+                    alt="First slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgFlash1}
+                    alt="Second slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgFlash}
+                    alt="Third slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true" />
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true" />
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
             <div class="card-body text-center">
               <h5 class="card-title">Flash Card App</h5>
               <p class="card-text">
-                In this project I have learnt how to make apps on both iOS and
-                Android platform by using React Native. In this project I learn
-                how to navigate to different screens, how to use stack navigator
-                and tab navigator. In addition to that I learnt how to use
-                Asyncstorage of a phone.
+                This project can work on both iOS and Android platform. I made
+                this project by using React Native. In this project user can
+                navigate to different screens, user can make their own cards
+                each card can contain unlimited number of quizes. user can
+                participate in quizes as well.
               </p>
               <a
                 href="https://github.com/zubair-mehboob/Flash-Card-App"
@@ -113,18 +360,77 @@ const Projects = () => {
         </div>
         <div className="col- col-md-6 col-lg-4 mb-4">
           <div class="card" style={{ height: "650px" }}>
-            <img
-              class="card-img-top"
-              src={imgb}
-              alt="Card image cap"
-              style={{ height: "300px" }}
-            />
+            <div
+              id="carouselExampleIndicators"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="0"
+                  class="active"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="1"
+                />
+                <li
+                  data-target="#carouselExampleIndicators"
+                  data-slide-to="2"
+                />
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img
+                    class="d-block w-100"
+                    src={imgLeads}
+                    alt="First slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgLeads1}
+                    alt="Second slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+                <div class="carousel-item">
+                  <img
+                    class="d-block w-100"
+                    src={imgLeads2}
+                    alt="Third slide"
+                    style={{ height: "300px" }}
+                  />
+                </div>
+              </div>
+              <a
+                class="carousel-control-prev"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="prev"
+              >
+                <span class="carousel-control-prev-icon" aria-hidden="true" />
+                <span class="sr-only">Previous</span>
+              </a>
+              <a
+                class="carousel-control-next"
+                href="#carouselExampleIndicators"
+                role="button"
+                data-slide="next"
+              >
+                <span class="carousel-control-next-icon" aria-hidden="true" />
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
             <div class="card-body text-center">
               <h5 class="card-title">Leads Group Coaching System Website</h5>
               <p class="card-text">
-                This is static single page website, in this project I learnt
-                various things about Bootstrap 4 like carousel, cards,
-                jumbotrons etc.
+                This is a static single page website, in this project I use
+                Bootstrap 4. This is a responsive website which can work well on
+                all devices.
               </p>
               <a
                 href="https://github.com/zubair-mehboob/LeadsGroupCoaching"
